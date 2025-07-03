141ce7eda86027355c8728ae7ea3b1cbe9959c1a
syedatayabakazmi <143135085+syedatayabakazmi@users.noreply.github.com>
2025-07-03 14:45:10 +0500

    Update NotificationService.java

--- notification-service/notification-service/src/main/java/com/quickcare/notification_service/service/NotificationService.java
+++ notification-service/notification-service/src/main/java/com/quickcare/notification_service/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.quickcare.notification_service.service;
 
+import com.quickcare.notification_service.dto.NotificationRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -20,24 +21,24 @@ public class NotificationService {
         mailSender.send(message);
     }
 
-    public void notifyAppointmentBooking(String patientEmail, String patientName, String doctorEmail, String doctorName, String appointmentDateTime) {
+    public void notifyAppointmentBooking(NotificationRequestDto notificationRequestDto) {
         // Email for Patient
-        sendEmailNotification(patientEmail, "Appointment Confirmed",
-                "Dear " + patientName + ", your appointment with Dr. " + doctorName + " is confirmed for " + appointmentDateTime);
+        sendEmailNotification(notificationRequestDto.getPatientEmail(), "Appointment Confirmed",
+                "Dear " + notificationRequestDto.getPatientName() + ", your appointment with Dr. " + notificationRequestDto.getDoctorName() + " is confirmed for " + notificationRequestDto.getAppointmentDateTime());
 
         // Email for Doctor
-        sendEmailNotification(doctorEmail, "New Appointment Scheduled",
-                "Dear Dr. " + doctorName + ", you have a new appointment with " + patientName + " on " + appointmentDateTime);
+        sendEmailNotification(notificationRequestDto.getDoctorEmail(), "New Appointment Scheduled",
+                "Dear Dr. " + notificationRequestDto.getDoctorName() + ", you have a new appointment with " + notificationRequestDto.getPatientName() + " on " + notificationRequestDto.getAppointmentDateTime());
     }
 
 
-    public void notifyAppointmentCancellation(String patientEmail, String patientName, String doctorEmail, String doctorName, String appointmentDateTime) {
+    public void notifyAppointmentCancellation(NotificationRequestDto notificationRequestDto) {
         // Email for Patient
-        sendEmailNotification(patientEmail, "Appointment Canceled",
-                "Dear " + patientName + ", your appointment with Dr. " + doctorName + " has been cancelled for " + appointmentDateTime);
+        sendEmailNotification(notificationRequestDto.getPatientEmail(), "Appointment Canceled",
+                "Dear " + notificationRequestDto.getPatientName() + ", your appointment with Dr. " + notificationRequestDto.getDoctorName() + " has been cancelled for " + notificationRequestDto.getAppointmentDateTime());
 
         // Email for Doctor
-        sendEmailNotification(doctorEmail, "Appointment Canceled",
-                "Dear Dr. " + doctorName + ", you have cancelled appointment with " + patientName + " on " + appointmentDateTime);
+        sendEmailNotification(notificationRequestDto.getDoctorEmail(), "Appointment Canceled",
+                "Dear Dr. " + notificationRequestDto.getDoctorName() + ", you have cancelled appointment with " +notificationRequestDto.getPatientName() + " on " + notificationRequestDto.getAppointmentDateTime());
     }
 }
